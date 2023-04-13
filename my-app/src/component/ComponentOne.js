d27c7a9c88f5f1b10135a6a2dd756fd295648f65
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-04-13 23:24:38 +0800

    Update ComponentOne.js

--- my-app/src/component/ComponentOne.js
+++ my-app/src/component/ComponentOne.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { PokemonList } from '../assets/PokemonList'
 import axios from 'axios'
 
 const ComponentOne = () => {
   const [pokemon, setPokemon] = useState([])
 
-  axios.get('https://pokeapi.co/api/v2/pokemon').then( res => {
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon').then( res => {
     setPokemon(res.data.results.map(p => (
       <li>
         {p.name}
       </li>
     )))
   })
+  }, [])
+  
 
   return (
     <div><PokemonList pokemon={pokemon} /></div>

b5fa863fa2345cc4b261fb8ccf556141bcc7e27d
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-04-14 16:03:59 +0800

    Update ComponentTwo.js

--- my-app/src/component/ComponentTwo.js
+++ my-app/src/component/ComponentTwo.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ComponentTwo = () => {
   return (
-    <div>ComponentTwo</div>
+    <>
+      <Link to='/'>
+      <h1>Home</h1>
+      </Link>
+      <div>ComponentTwo</div>
+    </>
   )
 }
 
